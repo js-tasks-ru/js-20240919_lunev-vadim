a14803dd9767ca63ecbccefda3f7b8023e37c0cf
vadim <lunev82@gmail.com>
2024-09-24 17:47:01 +0300

    2/1. Create function that sorts array of string by two criteria "asc" or "desc"

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -4,6 +4,21 @@
  * @param {string} [param="asc"] param - the sorting type "asc" or "desc"
  * @returns {string[]}
  */
-export function sortStrings(arr, param = 'asc') {
 
-}
+export const sortStrings = (arr, param = "asc") => {
+
+  const locales = ["ru", "en"];
+  const options = {caseFirst: "upper"};
+
+  if (param === "asc") {
+    return arr.toSorted((a, b) => {
+      return (a).localeCompare(b, locales, options);
+    });
+  } else if (param === "desc") {
+    {
+      return arr.toSorted((a, b) => {
+        return (b).localeCompare(a, locales, options);
+      });
+    }
+  }
+};
