f9bddfca2595343d283144412ea189da7005600b
vadim <lunev82@gmail.com>
2024-10-02 23:51:42 +0300

    2/2-3. Create function that filtered object keys by array of strings

--- 02-javascript-data-types/3-omit/index.js
+++ 02-javascript-data-types/3-omit/index.js
@@ -4,6 +4,7 @@
  * @param {...string} fields - the properties paths to omit
  * @returns {object} - returns the new object
  */
-export const omit = (obj, ...fields) => {
 
+export const omit = (obj, ...fields) => {
+  return Object.fromEntries((Object.entries(obj).filter(([key]) => !fields.includes(key))));
 };
