a34b6c9ed49789f4b7a0dff7c6799915cdc1e754
vadim <lunev82@gmail.com>
2024-10-11 11:13:52 +0300

    3/1-4 tasks

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,20 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
-
+  return (obj) => {
+    if (Object.keys(obj).length) {
+      const pathArray = path.split('.');
+      return pathArray.reduce((acc, key) => acc[key], obj);
+    }
+  };
 }
+
+const product = {
+  category: {
+    title: "Goods"
+  }
+};
+
+const getter = createGetter('category.title');
+
+console.log(getter(product)); // Goods
